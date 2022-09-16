d867c4b6ce876dabfa8cde1a3be1a7dcf570642c
Marco Heinemann <marco.heinemann@useblocks.com>
2022-09-16 17:17:00 +0200

    Added defaults

---
+++ sphinx_needs_modeling/modeling/defaults.py
@@ -0,0 +1,12 @@
+NEEDS_PYDANTIC_REMOVE_FIELDS = [
+    "docname",
+    "external_css",
+    "hide",
+    "is_modified",
+    "layout",
+    "style",
+    "type_color",
+    "type_name",
+    "type_prefix",
+    "type_style",
+]
