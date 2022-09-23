1837f4b7510e573261088d2cf4517945cefbfcf0
Marco Heinemann <marco.heinemann@useblocks.com>
2022-09-23 15:52:35 +0200

    Added comments

--- sphinx_modeling/logging.py
+++ sphinx_modeling/logging.py
@@ -1,6 +1,8 @@
+"""Provide the Sphinx logger."""
 from sphinx.util import logging
 from sphinx.util.logging import SphinxLoggerAdapter
 
 
 def get_logger(name: str) -> SphinxLoggerAdapter:
+    """Return a named Sphinx logger."""
     return logging.getLogger(name)
